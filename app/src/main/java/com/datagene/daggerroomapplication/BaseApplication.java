b4d4942ad819e07bccb435b392f5d151183c0f20
AzureAD\RafiSadat <rsadat@datagene.com.au>
2020-01-14 10:16:31 +1100

    App Component

---
+++ app/src/main/java/com/datagene/daggerroomapplication/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.datagene.daggerroomapplication;
+
+import com.datagene.daggerroomapplication.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
